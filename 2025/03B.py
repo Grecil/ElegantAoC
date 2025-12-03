b241b21739e94206a9e7e120f1e3e66cca0202a8
Grecil <grecilunadkat@gmail.com>
2025-12-03 11:04:21 +0530

    removed one variable

--- 2025/03B.py
+++ 2025/03B.py
@@ -6,8 +6,7 @@ for i in inp:
     arr = []
     for j in range(11, 0, -1):
         arr.append(max(i[:-j]))
-        k = i[:-j].index(arr[-1])
-        i = i[k + 1 :]
+        i = i[i.index(arr[-1]) + 1 :]
     arr.append(max(i))
     ans += int("".join(arr))
 print(ans)
