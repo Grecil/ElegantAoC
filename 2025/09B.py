adc2272f4bc44e446e7b1fbae1057b2eb75baa27
Grecil <grecilunadkat@gmail.com>
2025-12-09 11:51:06 +0530

    Added comments for clarity

--- 2025/09B.py
+++ 2025/09B.py
@@ -2,12 +2,16 @@ import sys
 
 rects = [tuple(map(int, i.split(",")))[::-1] for i in sys.stdin.readlines()]
 n = len(rects)
+
+# Coordinate Compression
 xst, yst = set(), set()
 for x, y in rects:
     xst.add(x)
     yst.add(y)
 x2i = {x: i + 1 for i, x in enumerate(sorted(xst))}
 y2i = {y: i + 1 for i, y in enumerate(sorted(yst))}
+
+# Creating the boundary
 lx, ly = len(x2i) + 2, len(y2i) + 2
 grid = [[-1] * ly for i in range(lx)]
 for i in range(len(rects)):
@@ -19,6 +23,8 @@ for i in range(len(rects)):
     else:
         for j in range(min(x1, x2), max(x1, x2) + 1):
             grid[j][y1] = 1
+
+# Making everything outside the boundary 0
 stk = [(0, 0)]
 while stk:
     x, y = stk.pop()
@@ -27,16 +33,22 @@ while stk:
         if -1 < nx < lx and -1 < ny < ly and grid[nx][ny] == -1:
             grid[nx][ny] = 0
             stk.append((nx, ny))
+
+# Making everything inside the boundary 1
 for i in range(lx):
     for j in range(ly):
         if grid[i][j] == -1:
             grid[i][j] = 1
+
+# 2D prefix sum to check if a rectangle is completely filled
 pre = [[0] * (ly + 1) for _ in range(lx + 1)]
 for i, row in enumerate(grid):
     pi, pii = pre[i], pre[i + 1]
     for j in range(ly):
         pre[i + 1][j + 1] = row[j] + pre[i][j + 1] + pre[i + 1][j] - pre[i][j]
 csum = lambda a, b, x, y: pre[x][y] - pre[x][b] - pre[a][y] + pre[a][b]
+
+# Make rectangles, check if they are filled and find the max area
 ans = 0
 for i in range(n - 1):
     x1, y1 = rects[i]
