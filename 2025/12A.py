aa272294e70d3d217b938ce5c73bae8b507bc8f7
Grecil <grecilunadkat@gmail.com>
2025-12-12 11:16:48 +0530

    added formatting

--- 2025/12A.py
+++ 2025/12A.py
@@ -1,11 +1,12 @@
 import sys
-inp=sys.stdin.read().split("\n\n")
-grids=inp.pop()
-ans=0
+
+inp = sys.stdin.read().split("\n\n")
+grids = inp.pop()
+ans = 0
 for row in grids.splitlines():
-    size,freq=row.split(": ")
-    a,b=map(int,size.split("x"))
-    tot=sum(map(int,freq.split()))
-    if tot*9<=a*b:
-        ans+=1
+    size, freq = row.split(": ")
+    a, b = map(int, size.split("x"))
+    tot = sum(map(int, freq.split()))
+    if tot * 9 <= a * b:
+        ans += 1
 print(ans)
